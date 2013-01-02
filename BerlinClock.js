f8d097d01ee9afc408dcbb21dfa6359f7a7b17a1
mdix <dix@mediaman.de>
2013-01-02 15:07:50 +0100

    Refactored getHours, not working ATM (check string access via array notation)

--- BerlinClock.js
+++ BerlinClock.js
@@ -1,25 +1,21 @@
 var berlinClock = function() {
-    var getHours = function(/*string*/ hours) {
-        var number = parseInt(hours);
-        var fives = Math.floor(number / 5);
-        var ones = number % 5;
-        var onesText = '';
-        for(x=0; x<4; x++){
-            if(x < fives){
-                onesText += 'R';
-            }else{
-                onesText += 'O';
-            }
+    var getHours = function(/*string*/ date) {
+        var hours = date.substr(0, 2);
+        hours = parseInt(hours);
+        var fives = Math.floor(hours / 5);
+        var ones = hours % 5;
+
+        var fivesText = "RRRR";
+        for(x=fives; x<fivesText.length; x++){
+            fivesText[x] = 'O';
         }
-        onesText += '\n';
-        for(x=0; x<4; x++){
-            if(x < ones){
-                onesText += 'R';
-            }else{
-                onesText += 'O';
-            }
+
+        var onesText = "RRRR";
+        for(x=ones; x<onesText.length; x++){
+            onesText[x] = 'O';
         }
-        return onesText;
+
+        return fivesText + '\n' + onesText;
     };
     var getMinutes = function(/*string*/ min) {
         var number = parseInt(min);
@@ -54,7 +50,7 @@ var berlinClock = function() {
 
     return {
         formatDate: function(/*string*/ date) {
-            return getSeconds(date) + "\n" + getHours(date.substr(0, 2)) + "\n" + getMinutes(date.substr(3, 2));
+            return getSeconds(date) + "\n" + getHours(date) + "\n" + getMinutes(date.substr(3, 2));
         }
     }
 };
