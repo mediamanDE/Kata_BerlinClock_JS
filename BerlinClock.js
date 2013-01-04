7948bb346b9c78fd028dd5481dc8588151c029dc
mdix <dix@mediaman.de>
2013-01-04 11:16:08 +0100

    first refactor try

--- BerlinClock.js
+++ BerlinClock.js
@@ -1,48 +1,40 @@
-var berlinClock = function() {
-    var getHours = function(/*string*/ date) {
-        var hours = date.substr(0, 2);
-        hours = parseInt(hours);
-        var fives = Math.floor(hours / 5);
-        var ones = hours % 5;
+String.prototype.replaceCharAtPosition = function(position, newChar) {
+    var stringArray = this.split('');
+    stringArray[position] = newChar;
+    return stringArray.join('');
+}
 
-        var fivesText = "RRRR";
+var berlinClock = function() {
+
+    var formatString = function(/*int*/ time, /*string*/ fivesText, /*string*/ onesText) {
+        var fives = Math.floor(time / 5);
+        var ones = time % 5;
+
         for(x=fives; x<fivesText.length; x++){
-            fivesText[x] = 'O';
+            fivesText = fivesText.replaceCharAtPosition(x, 'O');
         }
 
-        var onesText = "RRRR";
         for(x=ones; x<onesText.length; x++){
-            onesText[x] = 'O';
+            onesText = onesText.replaceCharAtPosition(x, 'O');
         }
 
         return fivesText + '\n' + onesText;
     };
-    var getMinutes = function(/*string*/ min) {
-        var number = parseInt(min);
-        var fives = Math.floor(number / 5);
-        var ones = number % 5;
-        var onesText = '';
-        for(x=1; x<=11; x++){
-            if(x <= fives){
-                if ((x % 3) == 0) {
-                    onesText += 'R';
-                } else {
-                    onesText += 'Y';
-                }
-            }else{
-                onesText += 'O';
-            }
-        }
-        onesText += '\n';
-        for(x=0; x<4; x++){
-            if(x < ones){
-                onesText += 'Y';
-            }else{
-                onesText += 'O';
-            }
-        }
-        return onesText;
+
+    var getHours = function(/*string*/ date) {
+        var hours = date.substr(0, 2);
+        hours = parseInt(hours);
+
+        return formatString(hours, "RRRR", "RRRR");
     };
+
+    var getMinutes = function(/*string*/ date) {
+        var hours = date.substr(3, 2);
+        hours = parseInt(hours);
+
+        return formatString(hours, "YYRYYRYYRYY", "YYYY");
+    };
+
     var getSeconds = function(/*string*/ date) {
         var seconds = date.substr(6, 2);
         return parseInt(seconds) % 2 === 1 ? "O" : "Y";
@@ -50,7 +42,7 @@ var berlinClock = function() {
 
     return {
         formatDate: function(/*string*/ date) {
-            return getSeconds(date) + "\n" + getHours(date) + "\n" + getMinutes(date.substr(3, 2));
+            return getSeconds(date) + "\n" + getHours(date) + "\n" + getMinutes(date);
         }
     }
 };
